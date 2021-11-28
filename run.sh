5fc34ea2afa0a0bcb97a7594670a78f369b52645
phil <phild@protonmail.com>
2021-11-28 08:49:34 +0000

    added entrypoint script to handle sourcing

--- run.sh
+++ run.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-docker run -it --rm app_template "$@"
+# For default we share the network with the host as well as the display to allow GUI applications.
+# The container is started interactively and removed when finished
+# Add additional flags if required.
+docker run \
+-it --rm \
+--network=host \
+--cap-add=SYS_PTRACE \
+--security-opt=seccomp:unconfined \
+--security-opt=apparmor:unconfined \
+--volume=/tmp/.X11-unix:/tmp/.X11-unix \
+app_template "$@"
